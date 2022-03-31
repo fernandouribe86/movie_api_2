d818064e3f4e611e6d24cce51037d9031d43737b
Fernando A. Uribe <fauribe56@gmail.com>
2022-03-30 21:05:42 -0700

    updated index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get("/", (req, res) => {
 // 	  });
 //    });
 //TEMPORARY CODE
-app.get("/movies", function (req, res) {
+app.get('/movies', function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies);
